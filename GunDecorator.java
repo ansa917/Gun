27d95549a4a4616922a9dd9266aa4aa3e6c1d673
Ansa Saghir <71321945+ansa917@users.noreply.github.com>
2022-04-22 14:27:48 +0100

    Update GunDecorator.java

--- GunDecorator.java
+++ GunDecorator.java
@@ -1,6 +1,7 @@
 package com.mycompany.gun;
 
+//Refactor the class name
 public abstract class GunDecorator extends Gun {
-
+// added description for gun extension
 	public abstract String getDescription();
 }
