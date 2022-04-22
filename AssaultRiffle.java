250e4ed2c0f8456ee7417c7ccd5ccb23e91a5bcc
Ansa Saghir <71321945+ansa917@users.noreply.github.com>
2022-04-22 14:29:55 +0100

    Update AssaultRiffle.java
    description added for extended class

--- AssaultRiffle.java
+++ AssaultRiffle.java
@@ -1,9 +1,10 @@
 package com.mycompany.gun;
-
+// created extend class for main class which is Gun
 public class AssaultRiffle extends Gun {
 	public AssaultRiffle() {
 		description = "Assault Riffle Gun";
 	}
+	// included the weight for assaul Eiffle gun
  
 	public double weight() {
 		return 5;
